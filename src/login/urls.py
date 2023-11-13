1192ced72136b381b32e3e3b32b00dee9cc867c4
kole <khoa.fsu@gmail.com>
2023-11-12 19:19:52 -0500

    signup

--- src/login/urls.py
+++ src/login/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.login_view, name='api-login'),
     path('logout/', views.logout_view, name='api-logout'),
     path('session/', views.session_view, name='api-session'),
+    path('signup/', views.signup_view, name='api-signup'),
     # path('whoami/', views.whoami_view, name='api-whoami'),
 ]
