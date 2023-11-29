cb2e7896d049efff24665aaf5246f21b296e4aa3
James Elliott <ellijr07@pfw.edu>
2023-11-28 21:40:47 -0500

    fix chatconsumer

--- src/chat/chatconsumer.py
+++ src/chat/chatconsumer.py
@@ -21,7 +21,6 @@ class ChatConsumer(WebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f'chat_{self.room_name}'
         self.room = ChatRoom.objects.get_or_create(name=self.room_name)[0]
-        print(self.room.id)
         self.ide = IDE.objects.get_or_create(chat_room_id=self.room.id)
         self.user = self.scope['user']
 
@@ -38,23 +37,30 @@ class ChatConsumer(WebsocketConsumer):
             self.channel_name,
         )
 
+        # add user as online
+        self.room.online.add(self.user)
+
         # send the user list to the newly joined user
-        self.send(json.dumps({
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
             'type': 'user_list',
             'users': [user.username for user in self.room.online.all()],
-        }))
+            }
+        )
 
         #send notification
-        self.send(json.dumps(
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
             {
                 'type': 'user_join',
                 'user': self.user.username,
+                'message': f"{self.user.username} joined the chat"
             }
-        ))
+        )
         
         # send last 30 messages
         last_30 = [{'user':last.user.username,'content':last.content} for last in reversed(Message.objects.filter(room=self.room).order_by('-timestamp')[:30])]
-        print(last_30)
         self.send(json.dumps(
             {
                 'type': 'message_list',
@@ -76,12 +82,14 @@ class ChatConsumer(WebsocketConsumer):
         print("ChatConsumer: disconnect")
 
         #send notification
-        self.send(json.dumps(
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
             {
                 'type': 'user_leave',
                 'user': self.user.username,
+                'message': f"{self.user.username} has left the chat"
             }
-        ))
+        )
 
         # disconnect
         async_to_sync(self.channel_layer.group_discard)(
@@ -120,5 +128,37 @@ class ChatConsumer(WebsocketConsumer):
             Called when sending a message to chat room
         '''
         print("ChatConsumer: send_message")
+
+        self.send(text_data=json.dumps(event))
+    
+    def user_join(self, event):
+        '''
+            Called when user joins chat room
+        '''
+        print("ChatConsumer: user_join")
+
+        self.send(text_data=json.dumps(event))
+
+    def user_list(self, event):
+        '''
+            Returns the current users online
+        '''
+        print("ChatConsumer: user_list")
+
+        self.send(text_data=json.dumps(event))
+
+    def message_list(self, event):
+        '''
+            Returns the last 30 messages to chat
+        '''
+        print("ChatConsumer: message_list")
+
+        self.send(text_data=json.dumps(event))
+    
+    def user_leave(self, event):
+        '''
+            Returns the last 30 messages to chat
+        '''
+        print("ChatConsumer: message_list")
 
         self.send(text_data=json.dumps(event))
